ae65314762075a9a4feb4cc0da0cd5febf5b57a5
Skrywerbeer <liquidsquid1@gmail.com>
2023-02-04 12:44:46 +0200

    Added stop start button for game of life section.

---
+++ webcomponents_gameOfLife.js
@@ -0,0 +1,14 @@
+"use strict";
+(document.getElementById("toggleBtn")).addEventListener("click", function () {
+    const game = document.querySelector("game-of-life");
+    if (this.innerText === "run") {
+        this.innerText = "stop";
+        game.setAttribute("timer", "run");
+    }
+    else if (this.innerText === "stop") {
+        this.innerText = "run";
+        game.setAttribute("timer", "stop");
+    }
+});
+(document.getElementById("randomBtn")).addEventListener("click", function () {
+});
