52d10fbe5c1481e2b292d168d45d8edec2c60157
Skrywerbeer <liquidsquid1@gmail.com>
2024-01-11 01:28:52 +0200

    Fixed script not rerunning.

--- gallery.js
+++ gallery.js
@@ -1,5 +1,7 @@
 "use strict";
-const buttons = document.querySelectorAll("#loadingIndicators button");
-const slideShow = document.querySelector("#loadingIndicators object[is=\"slide-show\"]");
-buttons[0].addEventListener("click", () => { slideShow.previousSlide(); });
-buttons[1].addEventListener("click", () => { slideShow.nextSlide(); });
+{
+    const buttons = document.querySelectorAll("#loadingIndicators button");
+    const slideShow = document.querySelector("#loadingIndicators object[is=\"slide-show\"]");
+    buttons[0].addEventListener("click", () => { slideShow.previousSlide(); });
+    buttons[1].addEventListener("click", () => { slideShow.nextSlide(); });
+}
--- gallery.ts
+++ gallery.ts
@@ -2,8 +2,10 @@ interface SlideShow {
 	previousSlide: () => void;
 	nextSlide: () => void;
 }
-const buttons = document.querySelectorAll("#loadingIndicators button");
-const slideShow =
-	document.querySelector("#loadingIndicators object[is=\"slide-show\"]") as SlideShow;
-buttons[0].addEventListener("click", () => {slideShow.previousSlide();});
-buttons[1].addEventListener("click", () => {slideShow.nextSlide();});
+{
+	const buttons = document.querySelectorAll("#loadingIndicators button");
+	const slideShow =
+		document.querySelector("#loadingIndicators object[is=\"slide-show\"]") as SlideShow;
+	buttons[0].addEventListener("click", () => {slideShow.previousSlide();});
+	buttons[1].addEventListener("click", () => {slideShow.nextSlide();});
+}
